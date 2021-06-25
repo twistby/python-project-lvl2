82abaae6ec41ef4ffa399157abd1b620d834bfef
twistby <pref@outlook.com>
2021-06-25 10:12:55 +0200

    bug fix 2

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -4,7 +4,7 @@ import os
 
 import pytest
 
-from gendiff.scripts.gendiff import generate_diff
+from gendiff.gendiff import generate_diff
 from gendiff.templates.jsonlish import jsonlish
 from gendiff.templates.plain import plain
 
